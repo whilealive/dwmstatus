c2f174c7b310bf67fa1e8af773259a6d448ed106
whilealive <bruno.bischi@gmail.com>
2014-03-03 15:11:22 +0100

    rebegin

--- dwmstatus.c
+++ dwmstatus.c
@@ -5,144 +5,102 @@
  *          based on the suckless dwmstatus project under
  *          git://git.suckless.org/dwmstatus
  *
- * DATE     25.02.2014
+ * DATE     03.03.2014
  * OWNER    Bischofberger
  * ==================================================================
  */
 
+/* TODO:   - check for power supply AC on
+ */
 
-#define _BSD_SOURCE  /* getloadavg() */
-#include <unistd.h>  /* sleep() */
+#include <unistd.h>  /* sleep() ... */
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdarg.h>  /* va_list ... */
 #include <string.h>
-#include <strings.h>
-#include <sys/time.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
-char *tzutc = "UTC";
-char *tzberlin = "Europe/Berlin";
+#define BATT_NOW     "/sys/class/power_supply/BAT1/charge_now"
+#define BATT_FULL    "/sys/class/power_supply/BAT1/charge_full"
+#define BATT_STATUS  "/sys/class/power_supply/BAT1/status"
 
 static Display *dpy;
 
-char *
-smprintf(char *fmt, ...)
+char * smprintf(char *fmt, ...)
 {
-	va_list fmtargs;
-	char *ret;
-	int len;
+    va_list fmtargs;
+    char *ret;
+    int len;
 
-	va_start(fmtargs, fmt);
-	len = vsnprintf(NULL, 0, fmt, fmtargs);
-	va_end(fmtargs);
+    va_start(fmtargs, fmt);
+    len = vsnprintf(NULL, 0, fmt, fmtargs);
+    va_end(fmtargs);
 
-	ret = malloc(++len);
-	if (ret == NULL) {
-		perror("malloc");
-		exit(1);
-	}
+    ret = malloc(++len);
+    if (ret == NULL) {
+        perror("malloc");
+        exit(1);
+    }
 
-	va_start(fmtargs, fmt);
-	vsnprintf(ret, len, fmt, fmtargs);
-	va_end(fmtargs);
+    va_start(fmtargs, fmt);
+    vsnprintf(ret, len, fmt, fmtargs);
+    va_end(fmtargs);
 
-	return ret;
+    return ret;
 }
 
-/* set environment variable TZ (timezone) to value "tzname"
- * more info on TZ: http://www.cyberciti.biz/faq/linux-unix-set-tz-environment-variable/
- */
-void
-settz(char *tzname)
+char * getbattery()
 {
-	setenv("TZ", tzname, 1);
+    long full, now = 0;
+    char *status = malloc(sizeof(char)*12);
+    char s = '?';
+    FILE *fp = NULL;
+    if ((fp = fopen(BATT_NOW, "r"))) {
+        fscanf(fp, "%ld\n", &full);
+        fclose(fp);
+        fp = fopen(BATT_FULL, "r");
+        fscanf(fp, "%ld\n", &now);
+        fclose(fp);
+        fp = fopen(BATT_STATUS, "r");
+        fscanf(fp, "%s\n", status);
+        fclose(fp);
+        if (strcmp(status,"Charging") == 0)
+            s = '+';
+        if (strcmp(status,"Discharging") == 0)
+            s = '-';
+        if (strcmp(status,"Full") == 0)
+            s = '=';
+        return smprintf("%c%ld%%", s,(full/(now/100)));
+    }
+    else return smprintf("");
 }
 
-/* Maybe it would be better to use timedatectl on Arch? */
-char *
-mktimes(char *fmt, char *tzname)
-{
-	char buf[129];
-	time_t tim;
-	struct tm *timtm;
-
-	memset(buf, 0, sizeof(buf));
-	settz(tzname);
-	tim = time(NULL);
-	timtm = localtime(&tim);
-	if (timtm == NULL) {
-		perror("localtime");
-		exit(1);
-	}
-
-	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
-		fprintf(stderr, "strftime == 0\n");
-		exit(1);
-	}
-
-	return smprintf("%s", buf);
-}
-
-void
-setstatus(char *str)
+void setstatus(char *str)
 {
 	XStoreName(dpy, DefaultRootWindow(dpy), str);
 	XSync(dpy, False);
 }
 
-/* get system load averages */
-char *
-loadavg(void)
+int main(void)
 {
-	double avgs[3];
+    char *status;
+    char *bat;
 
-	if (getloadavg(avgs, 3) < 0) {
-		perror("getloadavg");
-		exit(1);
-	}
+    if (!(dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "dwmstatus: cannot open display.\n");
+        return 1;
+    }
 
-	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+    for (;;sleep(90)) {
+        bat = getbattery();
+        status = smprintf("[BAT %s]", bat);
+        setstatus(status);
+        free(bat);
+        free(status);
+    }
+
+    XCloseDisplay(dpy);
+
+    return 0;
 }
-
-int
-main(void)
-{
-	char *status;
-	char *avgs;
-	char *tmar;
-	char *tmutc;
-	char *tmbln;
-
-	if (!(dpy = XOpenDisplay(NULL))) {
-		fprintf(stderr, "dwmstatus: cannot open display.\n");
-		return 1;
-	}
-
-	for (;;sleep(90)) {
-		/* avgs = loadavg(); */
-		tmar = mktimes("%H:%M", tzargentina);
-		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
-
-		status = smprintf("A:%s U:%s %s", tmar, tmutc, tmbln);
-		/* status = smprintf("L:%s A:%s U:%s %s",
-				avgs, tmar, tmutc, tmbln); */
-		setstatus(status);
-		free(avgs);
-		free(tmar);
-		free(tmutc);
-		free(tmbln);
-		free(status);
-	}
-
-	XCloseDisplay(dpy);
-
-	return 0;
-}
-
