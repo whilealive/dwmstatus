14c51ed3d19eebba59c524d0d58059009eb89d59
whilealive <bruno.bischi@gmail.com>
2014-03-20 09:58:44 +0100

    moved to main

--- dwmstatus.c
+++ dwmstatus.c
@@ -6,16 +6,17 @@
  *          git://git.suckless.org/dwmstatus,
  *          and alsa stuff from https://github.com/Unia/dwmst
  *
- *          Don't need time in statusbar since tmux does that for me
+ *          Attention: dwmstatus is likely to crash when suspending 
+ *          computer while it is not in sleep() state. Therefore, 
+ *          we have to run a custom systemd service which kills 
+ *          (if necessary) and reruns it after waking up.
  *
- * DATE     18.03.2014
+ * DATE     20.03.2014
  * OWNER    Bischofberger
  * ==================================================================
  */
 
 /* TODO: - implement a gtk+ warning window if bat is really low
- *       - write as a daemon to prevent from crashing when suspending
- *       - maybe with time anyway...?
  */
 
 #include <unistd.h>  /* sleep() ... */
@@ -25,6 +26,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <alsa/asoundlib.h>
+#include <time.h>
 #include <dirent.h>  /* check directory for new files */
 
 #include <X11/Xlib.h>
@@ -38,12 +40,10 @@
 static Display *dpy;
 
 typedef struct {
-    bool checkbat;
-    bool checkvol;
     char *mail_fast;
     char *mail_uzh;
     char *mail_zhaw;
-} init;
+} mailbox;
 
 static void die(const char *errmsg)
 {
@@ -74,116 +74,126 @@ char * smprintf(char *fmt, ...)
     return ret;
 }
 
-char * getbattery(bool checkbat)
+char * getbattery()
 {
-    if (checkbat) {
-        long full, now = 0;
-        char *status = malloc(sizeof(char)*12);
-        char s = '?';
-        FILE *fp = NULL;
-        if ((fp = fopen(BATT_NOW, "r"))) {
-            fscanf(fp, "%ld\n", &full);
-            fclose(fp);
-            fp = fopen(BATT_FULL, "r");
-            fscanf(fp, "%ld\n", &now);
-            fclose(fp);
-            fp = fopen(BATT_STATUS, "r");
-            fscanf(fp, "%s\n", status);
-            fclose(fp);
-            if (strcmp(status,"Charging") == 0)
-                s = '+';
-            if (strcmp(status,"Discharging") == 0)
-                s = '-';
-            if (strcmp(status,"Full") == 0)
-                s = '=';
-            return smprintf("[bat %c%ld] ", s,(full/(now/100)));
+    long full, now = 0;
+    char *status = malloc(sizeof(char)*12);
+    char s = '?';
+    FILE *fp = NULL;
+    if ((fp = fopen(BATT_NOW, "r"))) {
+        fscanf(fp, "%ld\n", &full);
+        fclose(fp);
+        fp = fopen(BATT_FULL, "r");
+        fscanf(fp, "%ld\n", &now);
+        fclose(fp);
+        fp = fopen(BATT_STATUS, "r");
+        fscanf(fp, "%s\n", status);
+        fclose(fp);
+        if (strcmp(status,"Charging") == 0) {
+            s = '+';
         }
-        else
-            return smprintf("");
+        if (strcmp(status,"Discharging") == 0) {
+            s = '-';
+        }
+        if (strcmp(status,"Full") == 0) {
+            s = '=';
+        }
+        return smprintf("[bat %c%ld] ", s,(full/(now/100)));
     }
-    else  /* return empty string if machine does not support bat checking */
+    else {
         return smprintf("");
+    }
 }
 
-snd_mixer_t *initvol(bool checkvol)
+snd_mixer_t *initvol()
 {
     snd_mixer_t *handle;
 
-    if(checkvol) {
-        snd_mixer_open(&handle, 0);
-        snd_mixer_attach(handle, "default");
-        snd_mixer_selem_register(handle, NULL, NULL);
-        snd_mixer_load(handle);
-    }
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, "default");
+    snd_mixer_selem_register(handle, NULL, NULL);
+    snd_mixer_load(handle);
+
     return handle;
 }
 
-char * getvol(bool checkvol, snd_mixer_t *handle) {
-    if (checkvol) {
-        int mute = 0;
-        long vol = 0, max = 0, min = 0;
-        snd_mixer_elem_t *pcm_mixer, *max_mixer;
-        snd_mixer_selem_id_t *vol_info, *mute_info;
+char * getvol(snd_mixer_t *handle) 
+{
+    int mute = 0;
+    long vol = 0, max = 0, min = 0;
+    snd_mixer_elem_t *pcm_mixer, *max_mixer;
+    snd_mixer_selem_id_t *vol_info, *mute_info;
 
-        snd_mixer_handle_events(handle);
-        snd_mixer_selem_id_malloc(&vol_info);
-        snd_mixer_selem_id_malloc(&mute_info);
-        snd_mixer_selem_id_set_name(vol_info, "Master");
-        snd_mixer_selem_id_set_name(mute_info, "Master");
-        pcm_mixer = snd_mixer_find_selem(handle, vol_info);
-        max_mixer = snd_mixer_find_selem(handle, mute_info);
-        snd_mixer_selem_get_playback_volume_range(pcm_mixer, &min, &max);
-        snd_mixer_selem_get_playback_volume(pcm_mixer, 0, &vol);
-        snd_mixer_selem_get_playback_switch(max_mixer, 0, &mute);
-        snd_mixer_selem_id_free(vol_info);
-        snd_mixer_selem_id_free(mute_info);
+    snd_mixer_handle_events(handle);
+    snd_mixer_selem_id_malloc(&vol_info);
+    snd_mixer_selem_id_malloc(&mute_info);
+    snd_mixer_selem_id_set_name(vol_info, "Master");
+    snd_mixer_selem_id_set_name(mute_info, "Master");
+    pcm_mixer = snd_mixer_find_selem(handle, vol_info);
+    max_mixer = snd_mixer_find_selem(handle, mute_info);
+    snd_mixer_selem_get_playback_volume_range(pcm_mixer, &min, &max);
+    snd_mixer_selem_get_playback_volume(pcm_mixer, 0, &vol);
+    snd_mixer_selem_get_playback_switch(max_mixer, 0, &mute);
+    snd_mixer_selem_id_free(vol_info);
+    snd_mixer_selem_id_free(mute_info);
 
-        if(mute == 0)
-            return smprintf("[MUTE]");
-        return smprintf("[vol %d]", ROUND_UNSIGNED((vol*10.0)/max));
-        //return smprintf("%d%%", (vol * 100) / max);
+    if (mute == 0) {
+        return smprintf("[MUTE]");
     }
-    else
-        return smprintf("");
+    return smprintf("[vol %d]", ROUND_UNSIGNED((vol*10.0)/max));
+    //return smprintf("%d%%", (vol * 100) / max);
 }
 
-char *get_nmail(char *directory, char *label)
+char *get_nmail(char *directory)
 {
-    /* directory : Maildir path 
-    * return label : number_of_new_mails
-    */
+    /* directory : Maildir path */
     int n = 0;
     DIR* dir = NULL;
     struct dirent* rf = NULL;
 
     dir = opendir(directory);  /* try to open directory */
-    if (dir == NULL)
+    if (dir == NULL) {
         perror("");
-
+    }
     while ((rf = readdir(dir)) != NULL) {  /*count number of files */
-        if (strcmp(rf->d_name, ".") != 0 && strcmp(rf->d_name, "..") != 0)
+        if (strcmp(rf->d_name, ".") != 0 && strcmp(rf->d_name, "..") != 0) {
             n++;
+        }
     }
     closedir(dir);
 
-   return smprintf("%s%d", label, n);
+   return smprintf("%d", n);
 }
 
-init initstatus(char *machine)
+char *gettime()
 {
-    init tmp;
+    char outstr[20];
+    time_t t;
+    struct tm *tmp;
+
+    t = time(NULL);
+    tmp = localtime(&t);
+
+    if (tmp == NULL) {
+        die("localtime failure");
+    }
+    if (strftime(outstr, sizeof(outstr), "%R", tmp) == 0) {
+        die("strftime returned 0");
+    }
+    return smprintf("%s", outstr);
+}
+
+mailbox initmail(char *machine)
+{
+    mailbox tmp;
 
     if(!strcmp("--laptop", machine)) {
-        tmp.checkbat = true;
-        tmp.checkvol = true;
         tmp.mail_fast = "/home/laptop/Maildir/fastmail/INBOX/new";
         tmp.mail_uzh  = "/home/laptop/Maildir/uzh-pseudo/INBOX_uzh/new";
         tmp.mail_zhaw = "/home/laptop/Maildir/zhaw-pseudo/INBOX_zhaw/new";
         return tmp;
     }
     else if(!strcmp("--desktop", machine)) {
-        tmp.checkbat = false;
-        tmp.checkvol = false;
         tmp.mail_fast = "/home/desktop/Maildir/fastmail/INBOX/new";
         tmp.mail_uzh  = "/home/desktop/Maildir/uzh-pseudo/INBOX_uzh/new";
         tmp.mail_zhaw = "/home/desktop/Maildir/zhaw-pseudo/INBOX_zhaw/new";
@@ -203,40 +213,75 @@ void setstatus(char *str)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-        die("usage: dwmstatus [--laptop/--desktop]\n");
-    if (!(dpy = XOpenDisplay(NULL)))
+    if (!(dpy = XOpenDisplay(NULL))) {
         die("dwmstatus: cannot open display.\n");
+    }
+    if (argc != 2) {
+        die("usage: dwmstatus [--laptop/--desktop]\n");
+    }
+    if (!strcmp("--laptop", argv[1])) {
+        char *status;
+        char *bat;
+        char *vol;
+        char *time;
+        char *new_fastmail;
+        char *new_uzh;
+        char *new_zhaw;
 
-    init mach;
-    mach = initstatus(argv[1]);
+        snd_mixer_t *handle;
+        handle = initvol();
 
-    char *status;
-    char *bat;
-    char *new_fastmail;
-    char *new_uzh;
-    char *new_zhaw;
-    char *vol;
+        mailbox box;
+        box = initmail(argv[1]);
 
-    snd_mixer_t *handle;
-    handle = initvol(mach.checkvol);
+        for (;;sleep(INTERVAL)) {
+            bat = getbattery();
+            vol = getvol(handle);
+            time = gettime();
+            new_fastmail = get_nmail(box.mail_fast);
+            new_uzh= get_nmail(box.mail_uzh);
+            new_zhaw= get_nmail(box.mail_zhaw);
 
-    for (;;sleep(INTERVAL)) {
-        bat = getbattery(mach.checkbat);
-        vol = getvol(mach.checkvol, handle);
-        new_fastmail = get_nmail(mach.mail_fast, "");
-        new_uzh= get_nmail(mach.mail_uzh, "");
-        new_zhaw= get_nmail(mach.mail_zhaw, "");
+            status = smprintf("[mail %s|%s|%s] %s%s %s", new_fastmail, new_uzh, new_zhaw, bat, vol, time);
+            setstatus(status);
 
-        status = smprintf("[mail %s|%s|%s] %s%s", new_fastmail, new_uzh, new_zhaw, bat, vol);
-        setstatus(status);
+            free(new_zhaw);
+            free(new_uzh);
+            free(new_fastmail);
+            free(time);
+            free(vol);
+            free(bat);
+            free(status);
+        }
+    }
+    else if (!strcmp("--desktop", argv[1])) {
+        char *status;
+        char *time;
+        char *new_fastmail;
+        char *new_uzh;
+        char *new_zhaw;
 
-        free(bat);
-        free(vol);
-        free(new_fastmail);
-        free(new_uzh);
-        free(new_zhaw);
-        free(status);
+        mailbox box;
+        box = initmail(argv[1]);
+
+        for (;;sleep(INTERVAL)) {
+            time = gettime();
+            new_fastmail = get_nmail(box.mail_fast);
+            new_uzh= get_nmail(box.mail_uzh);
+            new_zhaw= get_nmail(box.mail_zhaw);
+
+            status = smprintf("[mail %s|%s|%s] %s", new_fastmail, new_uzh, new_zhaw, time);
+            setstatus(status);
+
+            free(new_zhaw);
+            free(new_uzh);
+            free(new_fastmail);
+            free(time);
+            free(status);
+        }
+    }
+    else {
+        die("usage: dwmstatus [--laptop/--desktop]\n");
     }
 
     XCloseDisplay(dpy);
