fdc7eeef97f2d6b1af2dd7ee5a809b57e8d01f0e
whilealive <bruno.bischi@gmail.com>
2014-03-31 16:04:48 +0200

    initial commit

--- src/dwmstatus.h
+++ src/dwmstatus.h
@@ -18,12 +18,28 @@
  */
 
 #define INTERVAL            60  /* seconds */
+
 #define BATT_NOW            "/sys/class/power_supply/BAT1/charge_now"
 #define BATT_FULL           "/sys/class/power_supply/BAT1/charge_full"
 #define BATT_STATUS         "/sys/class/power_supply/BAT1/status"
+
+#define MAIL_LAP_FAST       "/home/laptop/Maildir/fastmail/INBOX/new"
+#define MAIL_LAP_UZH        "/home/laptop/Maildir/uzh-pseudo/INBOX_uzh/new"
+#define MAIL_LAP_ZHAW       "/home/laptop/Maildir/zhaw-pseudo/INBOX_zhaw/new"
+
+#define MAIL_DESK_FAST      "/home/desktop/Maildir/fastmail/INBOX/new"
+#define MAIL_DESK_UZH       "/home/desktop/Maildir/uzh-pseudo/INBOX_uzh/new"
+#define MAIL_DESK_ZHAW      "/home/desktop/Maildir/zhaw-pseudo/INBOX_zhaw/new"
+
 #define ROUND_UNSIGNED(d)   ( (int) ((d) + ((d) > 0 ? 0.5 : -0.5)) )
 
-static void die();
+typedef struct {
+    char *mail_fast;
+    char *mail_uzh;
+    char *mail_zhaw;
+} mailbox;
+
+void die();
 char * smprintf(char *fmt, ...);
 char * getbattery();
 snd_mixer_t *initvol();
